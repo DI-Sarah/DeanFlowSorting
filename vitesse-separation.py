35483052f0ccb1562da72833a5a278d911f362a5
DI-Sarah <sarah.duclos-ivetich@etu.chimieparistech.psl.eu>
2020-06-20 12:24:29 +0200

    Nouveau code avec vitesse

---
+++ vitesse-separation.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 20 11:41:08 2020
+
+@author: duclosivetich
+"""
+import matplotlib.pyplot as plt 
+import numpy as np
+import math
+
+a=2e-6
+a2=6e-6
+rho=1000
+H=20e-6
+U=260e-3
+Q=260e-3/H
+W=100e-6
+pr0=[H/3,W/3]
+pphi=np.linspace(10*np.pi,0,100) 
+rc=np.linspace(0.003,0.00125,100)
+vphi=0.27                                         #Vitesse constante longitudinale
+dt=0.00001 
+vr=[0,0]
+m=1050*4/3*np.pi*(a/2)**3 
+m2=1050*4/3*np.pi*(a2/2)**3
+rc=0.003
+mu = 0.001
+pr0=50e-6
+pr02=50e-6
+
+
+def Ul(p,rho,U,a,H):
+    z2=p/H
+    Cl=2.18*z2-21.23*z2*z2*z2
+    return rho*U*U*a*a*a*Cl/(3*np.pi*mu*H*H)
+
+def metpas (pr0,a):
+    tabr=np.array([pr0])
+    for i in range(99):
+        pr0=pr0+Ul(pr0,rho,U,a,H)*dt
+        tabr=np.append(tabr,pr0)
+    return tabr
+    
+
+y=metpas(pr0,a)
+y2=metpas(pr02,a2)
+x=pphi
+plt.plot(x,y)
+plt.plot(x,y2,'c')
+plt.show()
