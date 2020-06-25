b3727e326fe53fef6333b2a02f36d97f62e38c18
DI-Sarah <sarah.duclos-ivetich@etu.chimieparistech.psl.eu>
2020-06-25 15:53:44 +0200

    Mise couleur pour rapport

--- separation-Bonne version.py
+++ separation-Bonne version.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
-a=2.3e-6
+a=1.9e-6
 a2=7.32e-6
 rho=1000
 H=30e-6
@@ -21,62 +21,73 @@ U=260e-3
 Uf=260e-3
 Q=260e-3/H
 W=100e-6
-pphi=np.linspace(0,0.05,5000) 
-rc=np.linspace(0.003,0.00125,5000)                         #Vitesse constante longitudinale
+pphi=np.linspace(0,0.5,5000) 
+rc=np.linspace(0.00305,0.0013,5000)                         #Vitesse constante longitudinale
 dt=0.00001
 m=1050*4/3*np.pi*(a/2)**3 
 m2=1050*4/3*np.pi*(a2/2)**3
 mu = 0.001
-pr0=-5e-6
-pr02=-5e-6
-vr=0
-vr2=0
-
-'''def Fl(p,rho,U,r,W):
-    z2=p/W
-    Cl=np.abs(np.sin(10*np.pi/3/W*p-5*np.pi/3))
-    if (z2<1/2 and z2>1/5) or z2>4/5 :
-        
-        return -rho*U*U*Cl*16*r*r*r*r/W/W
-        #return -rho*Uf*Uf*Cl*2*a*2*a*2*a*2*a/H/H
-    else :
-        return rho*U*U*Cl*16*r*r*r*r/W/W'''
-        
+pr0b=0.00004
+pr02b=0.000002
+vrb=0
+vr2b=0
+pr=rc[0]
+pr2=rc[0]
 
 def Fl(p,rho,U,W,r):
     z2=p/W
+    Cl=(np.sin(10*z2*np.pi))
+    return 27*np.pi*r*r*r*r*rho*U*U*Cl/(2*W*W)
+        
+def Flb(p,rho,U,W,r):
+    z2=p/W
     Cl=2.18*z2-21.23*z2*z2*z2
     return 27*np.pi*r*r*r*r*rho*U*U*Cl/(2*W*W)
+
+
     
 def Fd(p,mu,r,Rc,rho,Uf,H):
     De=rho*Uf*H/mu*np.sqrt(H/(2*Rc))
     return 5.4e-4*np.pi*mu*De**(1.63)*r
 
 
-def methodepas(pphi,pr0,pr02,vr,vr2):
-    tabr0=np.array([pr0])
-    tabr02=np.array([pr02])
-    tab_vr=np.array([vr])
-    tab_vr2=np.array([vr2])
+def methodepas(pphi,vrb,vr2b,pr0b,pr02b,pr,pr2):
+    tabr0b=np.array([pr0b])
+    tabr02b=np.array([pr02b])
+    tabr=np.array([pr])
+    tabr2=np.array([pr2])
     for i in range(4999): 
         rcb=rc[i]
-        vr= ( Fl(pr0,rho,Uf,W,a)-Fd(pr0,mu,a,rcb,rho,Uf,H))/m*dt
-        vr2= (Fl(pr02,rho,Uf,W,a2)-Fd(pr02,mu,a2,rcb,rho,Uf,H))/m2*dt              #Utilisation du PFD sur la particule 1
-        pr0=pr0+vr*dt
-        pr02=pr02+vr2*dt
-        tabr0=np.append(tabr0,pr0)
-        tabr02=np.append(tabr02,pr02)
-        tab_vr=np.append(tab_vr,vr)
-        tab_vr2=np.append(tab_vr2,vr2)
+        vrb=  (Flb(pr0b,rho,Uf,W,a)+Fd(pr0b,mu,a,rcb,rho,Uf,H))/m*dt
+        vr2b= (Flb(pr02b,rho,Uf,W,a2)+Fd(pr02b,mu,a2,rcb,rho,Uf,H))/m2*dt          #Utilisation du PFD sur la particule 1
+        pr0b=pr0b+vrb*dt
+        pr02b=pr02b+vr2b*dt
+        pr=pr0b+rcb
+        pr2=pr02b+rcb
+        tabr0b=np.append(tabr0b,pr0b)
+        tabr02b=np.append(tabr02b,pr02b)
+        tabr=np.append(tabr,pr)
+        tabr2=np.append(tabr2,pr2)
         
-    return tabr0,tabr02  
-        
-        
-pos,pos2=methodepas(pphi,pr0,pr02,vr,vr2)
-plt.plot(pphi,pos)
-plt.plot(pphi,pos2,'c')        
-        
-        
+    return tabr0b,tabr02b 
+
+
+def spirale():
+    tabt=np.linspace(10*np.pi,0,10001)
+    tabrint=np.linspace(0.003,0.00125,10001)
+    tabrext=tabrint+100e-6
+    tabxI=tabrint*np.cos(tabt)
+    tabyI=tabrint*np.sin(tabt)
+    tabxE=tabrext*np.cos(tabt)
+    tabyE=tabrext*np.sin(tabt)
+    plt.plot(tabxI,tabyI,'k')
+    plt.plot(tabxE,tabyE,'k')
+#spirale()       
+pos3,pos4=methodepas(pphi,vrb,vr2b,pr0b,pr02b,pr,pr2)
+plt.plot(pphi,pos3,'r')
+plt.plot(pphi,pos4,'y')
+plt.show()        
+       
         
         
         
--- vitesse-separation.py
+++ vitesse-separation.py
@@ -11,54 +11,38 @@ import math
 from scipy import *
 from scipy . integrate import odeint 
 
-a=2e-6
-a2=7e-6
+a=1.9e-6
+a2=5.32e-6
 rho=1000
-H=30e-6
 U=260e-3
-Q=260e-3/H
 W=100e-6
-pr0=[H/3,W/3]
-pphi=np.linspace(0,0.5,1000) 
+pphi=np.linspace(0,0.5,10000) 
 rc=np.linspace(0.003,0.00125,100)
 vphi=0.27                                         #Vitesse constante longitudinale
-dt=0.0005
+dt=0.001
 m=1050*4/3*np.pi*(a/2)**3 
 m2=1050*4/3*np.pi*(a2/2)**3
 mu = 0.001
-pr0=-0.0000001
-pr02=-0.000000001
+pr02=-0.000045
 
 
-def Ul(p,pphi,rho,U,a,H):
-    z2=p/H
+def Ul(p,pphi,rho,U,a,W):
+    z2=p/W
     Cl=2.18*z2-21.23*z2*z2*z2
-    return rho*U*U*a*a*a*Cl/(3*np.pi*mu*H*H)
+    return rho*U*U*a*a*a*Cl/(3*np.pi*mu*W*W)
 
 def metpas (pr0,a):
     tabr=np.array([pr0])
-    for i in range(999):
-        pr0=pr0+Ul(pr0,pphi,rho,U,a,H)*dt
+    for i in range(9999):
+        pr0=pr0+Ul(pr0,pphi,rho,U,a,W)*dt
         tabr=np.append(tabr,pr0)
     return tabr
 
 
-axes=plt.gca()
-axes.set_ylim(-15e-6,15e-6)   
-solution=odeint(Ul,pr0,pphi,args=(rho,U,a,H))
-solution2=odeint(Ul,pr02,pphi,args=(rho,U,a2,H))
-pos=solution[:,0]
-pos2=solution2[:,0]
-plt.plot(pphi,pos,'k')
-plt.plot(pphi,pos2,'r')
 
 
-
-
-ym=metpas(pr0,a)
 y2=metpas(pr02,a2)
 x=pphi
-plt.plot(x,ym,'c')
 plt.plot(x,y2,'g')
 plt.show()
 
